7fc7203285dcec8365e961b84ebb452a37150d31
W-L-W <lennie499@gmail.com>
2025-10-13 14:29:18 +0100

    added empty solver to get answers showing up on logs

--- scripts/toy_task.py
+++ scripts/toy_task.py
@@ -1,6 +1,34 @@
+import json
 from inspect_ai import task, Task
+from inspect_ai import solver
+from inspect_ai.scorer import Target, Score, accuracy, stderr, scorer
 from inspect_ai.dataset import Sample, MemoryDataset
-from inspect_ai.solver import generate
+from inspect_ai.solver import TaskState, generate
+
+
+@scorer(metrics=[accuracy(), stderr()])
+def identity_scorer():
+    """Scorer that puts model output as 'answer' such that it shows in .eval table."""
+
+    async def score(state: TaskState, target: Target) -> Score:
+        return Score(
+            value=0,
+            answer=state.output.completion,
+        )
+
+    return score
+
+
+@solver
+def save_output(jsonfile_path: str):
+    """Saves output to a json file, where uuid is the key and output is the value."""
+
+    async def save(state: TaskState, target: Target) -> TaskState:
+        with open(jsonfile_path, "w") as f:
+            json.dump({state.metadata["uuid"]: state.output.completion}, f)
+        return state
+
+    return save
 
 
 @task
@@ -17,5 +45,5 @@ def toy_task():
     return Task(
         dataset=dataset,
         solver=generate(),
-        scorer=None,
+        scorer=identity_scorer(),
     )
