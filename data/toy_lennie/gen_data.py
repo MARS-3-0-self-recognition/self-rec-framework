3409fa48a5b1053ba13c6251b0a4e2ebce38803a
W-L-W <lennie499@gmail.com>
2025-10-16 09:23:07 +0100

    created project uuid 'base'

--- data/toy_lennie/gen_data.py
+++ data/toy_lennie/gen_data.py
@@ -3,9 +3,10 @@
 #             Sample(input="Please tell me a story about your childhood.", id="story"),
 #             Sample(input="Write a haiku about Autumn in Shoreditch.", id="haiku"),
 
-import json
 import uuid
-import os
+from pathlib import Path
+
+from src.helpers.utils import save_json
 
 # generated once manually by Lennie in the terminal at start of project
 MY_DATASET_NAMESPACE = uuid.UUID("7bae975c-d5fb-416b-a803-4582e5ef1a2c")
@@ -22,8 +23,7 @@ if __name__ == "__main__":
         str(uuid.uuid3(MY_DATASET_NAMESPACE, sample)): sample for sample in samples
     }
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    save_path = os.path.join(dir_path, "questions.json")
+    dir_path = Path(__file__).parent
+    save_path = dir_path / "questions.json"
 
-    with open(save_path, "w") as f:
-        json.dump(samples_dict, f, indent=4)
+    save_json(samples_dict, save_path)
